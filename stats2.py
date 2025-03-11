1d930daa213d2e4918160a532e8c6c7a202d2e42
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2025-03-11 14:24:24 +0000

    t

--- stats2.py
+++ stats2.py
@@ -145,8 +145,7 @@ def main():
         data['year'] = pd.to_datetime(data['Start Date'], format='mixed').dt.year
         start_date,end_date = st.slider('Select Year:', min_value=1971, max_value=2025, value=(1971, 2025))
 
-        filtered_data2 = data[
-            (data['year'] >= start_date) & (data['year'] <= end_date)]
+        filtered_data2 = data[(data['year'] >= start_date) & (data['year'] <= end_date)]
 
 
         choice2 = st.multiselect('Pace or Spin:', ['Pace', 'Spin'])
